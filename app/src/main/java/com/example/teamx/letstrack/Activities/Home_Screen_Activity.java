e04543fe85a3dc8a1eaf7ebb00cbe49dc24f8346
Aranyak-Ghosh <b00060789@aus.edu>
2017-11-30 17:32:36 +0400

    Got rid of the layout in previous commit and added some code to handle non verified email and phone

--- app/src/main/java/com/example/teamx/letstrack/Activities/Home_Screen_Activity.java
+++ app/src/main/java/com/example/teamx/letstrack/Activities/Home_Screen_Activity.java
@@ -3,52 +3,78 @@ package com.example.teamx.letstrack.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.teamx.letstrack.Application.Primary_User;
 import com.example.teamx.letstrack.ExternalInterface.DatabaseHelper;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Home_Screen_Activity extends Activity implements View.OnClickListener {
 
     Button settings;
-    Button resendmail;
+    Button action;
+
+    TextView message;
 
     Primary_User current_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
 
         current_user = DatabaseHelper.readPreference(getSharedPreferences("Current_User", MODE_PRIVATE));
 
-        if (!FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()) {
-            setContentView(R.layout.verifyemail);
-            resendmail = (Button) findViewById(R.id.btnResendVerificationEmail);
+        message = (TextView) findViewById(R.id.txtmessage);
 
-            resendmail.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    FirebaseAuth.getInstance()
-                            .getCurrentUser()
-                            .sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+        action = (Button) findViewById(R.id.btnaction);
+
+        if (!FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()) {
+            message.setText(R.string.verifyemail);
+            action.setText("Resend verification Email");
+        } else if (!current_user.isPhone_verified()) {
+            message.setText(R.string.verifyphone);
+            action.setText("Resend code");
+        } else {
+            action.setVisibility(View.INVISIBLE);
+            action.setEnabled(false);
+            message.setVisibility(View.INVISIBLE);
+            message.setText("");
+        }
+
+        action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (message.getText().equals(R.string.verifyemail)) {
+                    FirebaseAuth.getInstance().getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void aVoid) {
-                            Toast.makeText(Home_Screen_Activity.this, "Email sent!", Toast.LENGTH_SHORT)
+                            Log.d("", "Verification email sent");
+                            Toast.makeText(Home_Screen_Activity.this, "Verification Email Sent!", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("", "Failed to send verification email");
+                            Toast.makeText(Home_Screen_Activity.this, "Failed to send verification email!", Toast.LENGTH_SHORT)
                                     .show();
                         }
                     });
-
+                } else if (message.getText().equals(R.string.verifyphone)) {
+                    current_user.getP_verification().sendVerificationtext();
+                    DatabaseHelper.writeSharedPreference(getSharedPreferences("Current_User", MODE_PRIVATE), current_user);
+                    startActivity(new Intent(Home_Screen_Activity.this, Verify_phone_activity.class));
                 }
-            });
-        } else if (!current_user.isPhone_verified())
-            ;
-        else
-            setContentView(R.layout.activity_home_screen);
-
+            }
+        });
 
 
         settings = (Button) findViewById(R.id.buttonSettings);
